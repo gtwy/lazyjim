35130aac039cfdebec3945c77824d3660864c760
James Watt <james@techdirect.io>
2025-05-27 14:55:14 -0400

    changing themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,55 @@
 return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000, -- Make sure it loads before others like lualine
-    config = function()
-      vim.cmd.colorscheme("rose-pine")
-    end,
-  },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		-- priority = 1000,
+		-- config = function()
+		--   vim.cmd.colorscheme("rose-pine")
+		-- end,
+	},
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		-- 	require("nordic").load()
+		-- end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			require("kanagawa").load()
+		end,
+	},
+	-- {
+	-- 	"ViViDboarder/wombat.nvim",
+	-- 	dependencies = { { "rktjmp/lush.nvim" } },
+	-- 	opts = {
+	-- 		ansi_colors_name = nil,
+	-- 	},
+	-- },
+	-- {
+	-- 	"dasupradyumna/midnight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- },
+	-- {
+	-- 	"blazkowolf/gruber-darker.nvim",
+	-- },
+	-- {
+	-- 	"navarasu/onedark.nvim",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("onedark").setup({
+	-- 			style = "warm",
+	-- 		})
+	-- 		require("onedark").load()
+	-- 	end,
+	-- },
+	-- {
+	-- 	"xiantang/darcula-dark.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 	},
+	-- },
 }
